14ddbee270ce82444b7dd2e25a684784d2ed5003
ccaitano <cheryl.caitano@gmail.com>
2022-10-17 17:36:21 -1000

    Add in src folders and files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Section />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// Here we import the Header.css file to grant access to some additional classNames
+import '../styles/Footer.css';
+
+// TODO: Create a styles object called "styles"
+
+function Footer() {
+  // TODO: Add a style attribute to header and the h1 element
+
+  return (
+    <header className="footer">
+      <h1>Footer Text</h1>
+    </header>
+  );
+}
+
+export default Footer;
