77c7cf88eb58e680ca0ff2f313a04e11774ec156
ccaitano <cheryl.caitano@gmail.com>
2022-10-23 10:30:48 -1000

    Add icons to footer for github, linkedin and email links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Section from "./components/Section";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 // Here we import the Header.css file to grant access to some additional classNames
 // import '../styles/Footer.css';
 
@@ -10,13 +15,29 @@ const styles = {
       textAlign: 'center',
       padding: '10px'
   },
+  contactIcons: {
+      justifyContent: 'space-evenly'
+  },
+  copyright: {
+    paddingTop: '10px',
+    justifyContent: 'center'
+  }
 }
 function Footer() {
   // TODO: Add a style attribute to header and the h1 element
 
   return (
     <footer className="footer" style={styles.footer}>
-      <h6>2022</h6>
+      <Container fluid>
+        <Row className="contactIcons" style={styles.contactIcons}>
+              <a href="https://github.com/ccaitano" target="_blank" rel="noreferrer"><GitHubIcon className="icon">GitHub Profile</GitHubIcon></a>
+              <a href="https://www.linkedin.com/in/cheryl-caitano-0a8a8250/" target="_blank" rel="noreferrer"><LinkedInIcon className="icon">LinkedIn Profile</LinkedInIcon></a>
+              <a href="mailto:cheryl.caitano@gmail.com" target="_blank" rel="noreferrer"><EmailIcon className="icon">E-mail</EmailIcon></a>
+        </Row>
+        <Row className="copyright" style={styles.copyright}>
+          <h6>© 2022</h6>
+        </Row>
+      </Container>
     </footer>
   );
 }
